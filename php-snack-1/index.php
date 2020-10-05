4b5c6f45bce9fd5ec7547fabed184a68c66f5a30
lorenzo-damico <loredam@hotmail.it>
2020-10-05 16:31:45 +0200

    Snack 1 completo

--- php-snack-1/index.php
+++ php-snack-1/index.php
@@ -1,4 +1,4 @@
-<!-- 
+<!--
 Creiamo un array 'matches' contenente altri array i quali rappresentano delle
 partite di basket di un’ipotetica tappa del calendario. Ogni array della partita
 avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di
@@ -9,6 +9,57 @@ Olimpia Milano - Cantù | 55-60
 
 <?php
 
+$matches = [
+  [
+    "Squadra di casa" => "Cantù",
+    "Squadra ospite" => "Pesaro",
+    "Punti casa" => 81,
+    "Punti ospite" => 72
+  ],
+  [
+    "Squadra di casa" => "Brindisi",
+    "Squadra ospite" => "Roma",
+    "Punti casa" => 92,
+    "Punti ospite" => 67
+  ],
+  [
+    "Squadra di casa" => "Fortitudo Bologna",
+    "Squadra ospite" => "Varese",
+    "Punti casa" => 83,
+    "Punti ospite" => 88
+  ],
+  [
+    "Squadra di casa" => "Sassari",
+    "Squadra ospite" => "Trieste",
+    "Punti casa" => 72,
+    "Punti ospite" => 74
+  ],
+  [
+    "Squadra di casa" => "Brescia",
+    "Squadra ospite" => "Virtus Bologna",
+    "Punti casa" => 80,
+    "Punti ospite" => 81
+  ],
+  [
+    "Squadra di casa" => "Olimpia Milano",
+    "Squadra ospite" => "Treviso",
+    "Punti casa" => 104,
+    "Punti ospite" => 64
+  ],
+  [
+    "Squadra di casa" => "Cremona",
+    "Squadra ospite" => "Venezia",
+    "Punti casa" => 66,
+    "Punti ospite" => 83
+  ],
+  [
+    "Squadra di casa" => "Trento",
+    "Squadra ospite" => "Reggiana",
+    "Punti casa" => 81,
+    "Punti ospite" => 87
+  ]
+];
+
 ?>
 
 <!DOCTYPE html>
@@ -18,6 +69,19 @@ Olimpia Milano - Cantù | 55-60
     <title>PHP Snack 1</title>
   </head>
   <body>
+    <h1>Risultati seconda giornata</h1>
+    <ul>
+      <?php
+        for ($i=0; $i < count($matches) ; $i++) {
+          $squadra_casa = ($matches[$i]["Squadra di casa"]);
+          $squadra_ospite = ($matches[$i]["Squadra ospite"]);
+          $punti_casa = ($matches[$i]["Punti casa"]);
+          $punti_ospite = ($matches[$i]["Punti ospite"]);
+
+          echo "<li>".$squadra_casa." - ".$squadra_ospite." | ".$punti_casa."-".$punti_ospite;
+        }
+      ?>
+    </ul>
 
   </body>
 </html>
